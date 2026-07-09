22fb3c2c133f923e4eeafea1c48c40d158ce16e8
huncijr <hunor.burian08@gmail.com>
2026-07-09 15:19:41 +0300

    started 3D kiss_game

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './Pages/home/home';
 import { Calendar } from './Pages/games/calendar/calendar';
 import { Account } from './Pages/account/account';
 import { GiftFinder } from './Pages/games/gift/gift';
+import { KissComponent } from './Pages/games/kiss/kiss_games';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'account', component: Account },
   { path: 'games/calendar', component: Calendar },
   { path: 'games/gift', component: GiftFinder },
+  { path: 'games/kiss-game', component: KissComponent },
 ];
